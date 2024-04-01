d23dc82d542e60d9c4c81b9b5dd4d19bc5af3be2
Cesar <cesar@revendamais.com.br>
2024-04-01 00:27:41 -0300

    new where deposit

--- app/Repository/DepositRepository.php
+++ app/Repository/DepositRepository.php
@@ -11,4 +11,15 @@ class DepositRepository extends AbstractRepository
 {
     protected static $model = Deposit::class;
 
+    public static function findByMonthAndYear(int $month, int $year):Collection|null{
+        return self::loadModel()::query()->whereMonth('created_at', $month)
+        ->whereYear('created_at', $year)
+        ->where('user_id', auth()->user()->id)
+        ->where('status', 'approved')
+        ->get();
+    }
+
+    public static function total(){
+        return self::loadModel()::query()->where('user_id', auth()->user()->id)->where('status', 'approved')->sum('amount');
+    }
 }
